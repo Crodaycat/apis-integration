452ccba36c77a5b676748b50999297964a272b82
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-03-23 12:10:50 -0500

    Fix: create quote service await for DB save, error handling

--- src/services/create-quote.js
+++ src/services/create-quote.js
@@ -1,15 +1,19 @@
-const getQuoteService = require('../apis/quotes-api/get-quote');
-const getImageService = require('../apis/images-api/get-image');
+const getQuoteService = require('../apis/get-quote');
+const getImageService = require('../apis/get-image');
 const createQuoteDao = require('../dao/create-quote-dao');
 
-const generateQuote = async (req, res) => {
-  const resQuote = await getQuoteService();
+const generateQuote = async (req, res, next) => {
+  try {
+    const resQuote = await getQuoteService();
 
-  const image = await getImageService(resQuote.quote);
+    const image = await getImageService(resQuote.quote);
 
-  const savedQuote = createQuoteDao(resQuote.quote, image);
+    const savedQuote = await createQuoteDao(resQuote.quote, image);
 
-  res.send(savedQuote);
+    res.send(savedQuote);
+  } catch (error) {
+    next(error, req, res, next);
+  }
 };
 
 module.exports = generateQuote;
